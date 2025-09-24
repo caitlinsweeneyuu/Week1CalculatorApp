bb63e7507c155437cbdd853628b2b4ffc4ae54df
caitl <sweeney-c33@ulster.ac.uk>
2025-09-24 15:26:28 +0100

    took input for 2 numbers and outputed their addition result to the screen

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Enter your first number");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter your second number");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = firstNumber + secondNumber;
+
+Console.WriteLine("Adding {0} and {1} give the answer {2}", firstNumber, secondNumber, result);
