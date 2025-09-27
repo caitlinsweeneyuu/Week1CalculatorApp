af5fa4bb8fd6dd8a7f602b08bf84ad11f85073dc
caitl <sweeney-c33@ulster.ac.uk>
2025-09-27 22:41:02 +0100

    updated program with if else statements

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,12 +1,58 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Xml.Serialization;
 
+calculatorApp();
 
-Console.WriteLine("Enter your first number");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+void calculatorApp() {
 
-Console.WriteLine("Enter your second number");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+    int firstNumber = 0;
+    int secondNumber = 0;
+    int result = 0;
+    int choice = 0;
 
-int result = firstNumber + secondNumber;
+    Console.WriteLine("Enter your first number");
+    firstNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Adding {0} and {1} give the answer {2}", firstNumber, secondNumber, result);
+    Console.WriteLine("Enter your second number");
+    secondNumber = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Choose an option from the following list:");
+    Console.WriteLine("1 - Add");
+    Console.WriteLine("2 - Subtract");
+    Console.WriteLine("3 - Multiply");
+    Console.WriteLine("4 - Divide");
+
+    choice = Convert.ToInt32(Console.ReadLine());
+
+    if (choice == 1)
+    {
+        result = firstNumber + secondNumber;
+        Console.WriteLine("Adding {0} and {1} give the answer {2}", firstNumber, secondNumber, result);
+    }
+    else if (choice == 2)
+    {
+        result = firstNumber - secondNumber;
+        Console.WriteLine("Subtracting {0} and {1} give the answer {2}", firstNumber, secondNumber, result);
+    }
+    else if (choice == 3)
+    {
+        result = firstNumber * secondNumber;
+        Console.WriteLine("Multiplying {0} and {1} give the answer {2}", firstNumber, secondNumber, result);
+    }
+    else if (choice == 4)
+    {
+        result = firstNumber / secondNumber;
+        Console.WriteLine("Dividing {0} and {1} give the answer {2}", firstNumber, secondNumber, result);
+    }
+    else
+    {
+        Console.WriteLine("Invalid choice");
+    }
+    {
+        
+    }
+
+    result = firstNumber + secondNumber;
+
+    Console.WriteLine("Adding {0} and {1} give the answer {2}", firstNumber, secondNumber, result);
+    }
